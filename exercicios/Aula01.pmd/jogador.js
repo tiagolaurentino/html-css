0c4f69af8b32912a7eb3782e89c980e0756e7354
TIAGO LAURENTINO DA CONCEICAO <108843416+tiagolaurentino@users.noreply.github.com>
2022-09-14 16:25:27 -0300

    exercício 26 005

--- exercicios/Aula01.pmd/jogador.js
+++ exercicios/Aula01.pmd/jogador.js
@@ -9,12 +9,26 @@ class Jogador {
         this.energia = energia
         this.envenenado = envenenado
     }
+    /*receberDano(valor) {
+        this.energia -= valor
+    }
+    
+    atacar(jogador) {
+        jogador.receberDano(2)
+    }
+    
+    receberCura(valor) {
+        this.energia += valor
+    } */
 }
 
 
-const jogador1 = new Jogador("João", 5, false)
-const jogador2 = new Jogador("Mateus", 9, true)
-const jogador3 = new Jogador("Marcos", 7, true)
+const jogador1 = new Jogador("João", 2, false)
+const jogador2 = new Jogador("Mateus", 10, true)
+const jogador3 = new Jogador("Marcos", 8, true)
+
+/*jogador1.atacar(jogador2)
+jogador2.receberCura(5)*/
 
 console.log(jogador1)
 console.log(jogador2)
